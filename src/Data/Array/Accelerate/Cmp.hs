ee7fdf21dd0269692518b3ec0dbc28f96b42eb6b
Andy Gill <andygill@ku.edu>
2013-12-14 16:27:15 -0600

    Adding CUDA compare

--- src/Data/Array/Accelerate/Cmp.hs
+++ src/Data/Array/Accelerate/Cmp.hs
@@ -1,9 +1,39 @@
+{-# LANGUAGE CPP #-}
 module Data.Array.Accelerate.Cmp where
 
-import Data.Array.Accelerate
+import Data.Array.Accelerate hiding (zip,(++),tail)
 import qualified Data.Array.Accelerate.Interpreter as I
 
--- First, get it working for the model
-run :: Arrays a => Acc a -> a
-run = I.run
+#ifdef CUDA
+import qualified Data.Array.Accelerate.CUDA as CUDA
+#endif
 
+runs :: Arrays a => [Acc a -> a]
+#ifdef CUDA
+runs = [I.run,CUDA.run]
+#else
+runs = [I.run,I.run]
+#endif
+
+class ArrCmp a where
+  arrEq :: a -> a -> Bool
+  arrShow :: a -> String
+
+instance (Eq a, Show a) => ArrCmp (Array sh a) where
+  arrEq a b = toList a == toList b
+  arrShow = show . toList
+
+run :: (ArrCmp a, Arrays a) => Acc a -> a
+run acc = report rss
+  where
+        ress = [ f acc | f <- runs ]
+        rss = ress `zip` tail ress
+
+        report [] = head ress
+        report ((x,y):xs)
+                | x `arrEq` y = report xs
+                | otherwise = error $ unlines
+                                [ "found different results in run"
+                                , arrShow x
+                                , arrShow y
+                                ]
